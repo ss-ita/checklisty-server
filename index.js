8d257cc6ec6d7cc33850ce8481d8e4156a719e46
Roman Kordyuk <kordukroman@gmail.com>
2019-01-30 14:48:09 +0200

    added pre-commit hook

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-
 const app = express();
+
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => res.json('App get works'));
 
-app.get('/', (req,res) => (res.json("App get works")));
-
-app.listen(port, ()=>console.log("Server is running on port " + port));
-
+app.listen(port, () => console.log('Server is running on port ' + port)); //eslint-disable-line
