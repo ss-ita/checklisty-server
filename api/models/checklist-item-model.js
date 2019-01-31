804a6be0fa39c90691f8fe7f3c0d2842ae0a3c55
Rostyslav Vasyliuk <35967041+rostyslav-vasyliuk@users.noreply.github.com>
2019-01-31 14:46:51 +0200

    #7 - Create checklist model

---
+++ api/models/checklist-item-model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const checklistItemSchema = new mongoose.Schema({
+    title: {type: String},
+    description: {type: String},
+    tag: [{type: String}],
+    category: {type: String},
+    //----------------------------------------------------------------------------
+    //------------------------Number type for priority----------------------------
+    //0 - low priority(green), 1 - medium priority(yellow), 2 - high priority(red)
+    //----------------------------------------------------------------------------
+    priority: {type: Number}
+ })
+
+const ChecklistItem = mongoose.model('ChecklistItem', checklistItemSchema);
+
+module.exports = ChecklistItem;
---
+++ api/models/checklist-model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const ChecklistItem = require('./checklist-item-model');
+
+const checklistSchema = new mongoose.Schema({
+    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    creation_date: {type: Date, default: Date.now()}, 
+    item_data: [ChecklistItem]
+})
+
+const Checklist = mongoose.model('Checklist', checklistSchema);
+
+module.exports = Checklist;
