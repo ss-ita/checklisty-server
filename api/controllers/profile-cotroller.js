434ff93d50dd908f00d0b24e43110a4260269d82
Rostyk Machikha <finessq@gmail.com>
2019-02-04 22:04:05 +0200

    add set operator to update method and chacnge parameters in find by id at get method

--- api/controllers/profile-cotroller.js
+++ api/controllers/profile-cotroller.js
@@ -3,7 +3,7 @@ const User = require('../models/user-model');
 const updateProfile = async (req, res) => {
     try {
         const { id, user } = req.body;
-        const updateUserInfo = await User.findByIdAndUpdate(id, { user });
+        const updateUserInfo = await User.findByIdAndUpdate(id, {$set: user });
 
         return res.status(200).json(updateUserInfo);
     } catch (err) {
@@ -14,7 +14,7 @@ const updateProfile = async (req, res) => {
 const getProfile = async (req, res) => {
     try{
         const { id } = req.body;
-        const {password, ...user} = await User.findById({ id });
+        const {password, ...user} = await User.findById(id);
         return res.status(200).json(user);
     } catch (err){
         res.status(500).json(err);
