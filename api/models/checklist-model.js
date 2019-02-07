ab405558156b5a08be3ad3b8612453a7f29ab44f
OBats <orest.bats@gmail.com>
2019-02-07 15:13:01 +0200

    Closes #27 Fix typos in checklists model and controller

--- api/models/checklist-model.js
+++ api/models/checklist-model.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const ChecklistItem = require('./checklist-item-model');
 
 const checklistSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    creation_date: {type: Date, default: Date.now()}, 
-    item_data: [ChecklistItem.checklistItemSchema]
+    title: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    creation_date: { type: Date, default: Date.now() },
+    items_data: [ChecklistItem.checklistItemSchema]
 });
 
 const Checklist = mongoose.model('Checklist', checklistSchema);
