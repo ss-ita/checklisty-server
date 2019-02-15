ff14946cc7d5ff4b5d782a499bd5ca94bfd41235
Yura <yura837pristay@gmail.com>
2019-02-15 19:06:48 +0200

    writing password checking

--- api/controllers/profile-cotroller.js
+++ api/controllers/profile-cotroller.js
@@ -1,10 +1,19 @@
 const { User } = require('../models/user-model');
+const bcrypt = require('bcryptjs');
 
 const updateProfile = async (req, res) => {
     try {
-        const { id, user } = req.body;
-        const updateUserInfo = await User.findByIdAndUpdate(id, {$set: user });
-
+        let user = req.body.values;
+        const updatedUser = {
+            username: user.username,
+            email: user.email,
+            password: user.newPassword
+        };
+        //const user1 = await User.findOne({ email });
+        //const validPassword = await bcrypt.compareSync(user.oldPassword, user1.password);
+        const salt = bcrypt.genSaltSync(10);
+        updatedUser.password = await bcrypt.hashSync(updatedUser.password, salt);
+        const updateUserInfo = await User.findByIdAndUpdate(req.userData._id, {$set: updatedUser });
         return res.status(200).json(updateUserInfo);
     } catch (err) {
         res.status(500).json(err);
@@ -13,8 +22,8 @@ const updateProfile = async (req, res) => {
 
 const getProfile = async (req, res) => {
     try{
-        const { id } = req.body;
-        const user = await User.findById(id).select("-password");
+        const user = await User.findById(req.userData._id).select("-password");
+        if (!user) res.status(404).json({message: "User not found!"});
 
         return res.status(200).json(user);
     } catch (err){
