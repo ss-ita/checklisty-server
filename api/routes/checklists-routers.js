7a8bb45a3b73ee80323824577e020477eb36e1ea
Rostyk Machikha <34648058+machikharostyk@users.noreply.github.com>
2019-02-15 19:05:14 +0200

    change author value to user name (#37)

--- api/routes/checklists-routers.js
+++ api/routes/checklists-routers.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = new express.Router();
 const checklistControler = require('../controllers/checklist-controler');
+const authCheck = require('../middlewares/auth-check');
 
 router.get('/', checklistControler.getAll);
-router.post('/create', checklistControler.createCheckList);
-router.post('/create/:id', checklistControler.createCheckListItem);
+router.post('/create', authCheck, checklistControler.createCheckList);
+router.post('/create/:id', authCheck, checklistControler.createCheckListItem);
 router.get('/:id', checklistControler.getOne);
 router.put('/:id', checklistControler.update);
 router.delete('/:id', checklistControler.deleteList);
