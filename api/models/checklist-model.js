97236438922dfa86bfc7804987d38c8d37a70615
Rostyslav Vasyliuk <35967041+rostyslav-vasyliuk@users.noreply.github.com>
2019-02-20 13:36:52 +0200

    Closes #45 - Update checklist model

--- api/models/checklist-model.js
+++ api/models/checklist-model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const ChecklistItem = require('./checklist-item-model');
+const Section = require('./section-model');
 
 const checklistSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, ref: 'User' },
     creation_date: { type: Date, default: Date.now() },
-    items_data: [ChecklistItem.checklistItemSchema]
+    items_data: [Section.sectionSchema]
 });
 
 const Checklist = mongoose.model('Checklist', checklistSchema);
