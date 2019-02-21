1d9a9928fbfc58a784c06a1b6fa193f21b968e2f
Anzhelika <angelika0094@gmail.com>
2019-02-21 13:04:11 +0200

    Closes #41 Update backend for checklist-section model (#47)

--- api/models/checklist-model.js
+++ api/models/checklist-model.js
@@ -3,9 +3,9 @@ const Section = require('./section-model');
 
 const checklistSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    author: { type: String, ref: 'User' },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     creation_date: { type: Date, default: Date.now() },
-    items_data: [Section.sectionSchema]
+    sections_data: [Section.sectionSchema]
 });
 
 const Checklist = mongoose.model('Checklist', checklistSchema);
