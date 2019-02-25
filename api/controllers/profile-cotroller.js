33e6a6f9e84a8407f13bc32f9f83290208155a3e
Yura <yura837pristay@gmail.com>
2019-02-25 17:32:15 +0200

    Optimized updating profile info to database

--- api/controllers/profile-cotroller.js
+++ api/controllers/profile-cotroller.js
@@ -1,30 +1,17 @@
 const { User } = require('../models/user-model');
 const bcrypt = require('bcryptjs');
-const uniqueValidator = require('mongoose-unique-validator');
-const mongoose = require('mongoose');
 
 const updateProfile = async (req, res) => {
     try {
         const userId = req.userData.id;
         const user = await User.findById(userId);
-        const userSchema = mongoose.Schema({
-            username: { type: String, unique: true },
-            email: { type: String, index: true, unique: true },
-        });
-        userSchema.plugin(uniqueValidator);
-        if (user.username === req.body.username) await User.findByIdAndUpdate(
+        const userParams = { username: req.body.username, email: req.body.email };
+        if (user.username === req.body.username) delete userParams.username;
+        if (user.email === req.body.email) delete userParams.email;
+        if(!Object.keys(userParams).length) return res.status(409).json({message: "Please fill the form!"});
+        await User.findByIdAndUpdate(
             userId,
-            { email: req.body.email },
-            { runValidators: true, context: 'query' }
-        );
-        else if (user.email === req.body.email) await User.findByIdAndUpdate(
-            userId,
-            { username: req.body.username },
-            { runValidators: true, context: 'query' }
-        );
-        else await User.findOneAndUpdate(
-            userId,
-            { username: req.body.username, email: req.body.email },
+            {$set: userParams},
             { runValidators: true, context: 'query' }
         );
         res.status(200).json({ message: 'Name and email changed!' });
