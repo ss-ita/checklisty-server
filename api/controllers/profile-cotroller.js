983feebf8c35ac883839588b6c23a003ef58e54d
Yura <yura837pristay@gmail.com>
2019-02-28 16:14:32 +0200

    Added validation to profile controller

--- api/controllers/profile-cotroller.js
+++ api/controllers/profile-cotroller.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user-model');
+const { User, validate } = require('../models/user-model');
 const bcrypt = require('bcryptjs');
 
 const updateProfile = async (req, res) => {
@@ -6,15 +6,17 @@ const updateProfile = async (req, res) => {
         const userId = req.userData.id;
         const user = await User.findById(userId);
         const userParams = { username: req.body.username, email: req.body.email };
+        const { error } = validate(req.body);
+        if (error) return res.status(400).json({message: error.details[0].message});
         if (user.username === req.body.username) delete userParams.username;
         if (user.email === req.body.email) delete userParams.email;
         if(!Object.keys(userParams).length) return res.status(409).json({message: "Please fill the form!"});
-        await User.findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
             userId,
             {$set: userParams},
             { runValidators: true, context: 'query' }
-        );
-        res.status(200).json({ message: 'Name and email changed!' });
+        ).select('-password');
+        res.status(200).json({ updatedUser, message: 'Name and email changed!' });
     } catch (err) {
         if (err.name === 'ValidationError') res.status(409).json(err);
         else res.status(500).json(err);
@@ -36,6 +38,10 @@ const updateUserPassword = async (req, res) => {
     try{
         const userId = req.userData.id;
         const user = await User.findById(userId);
+        const { error } = validate(req.body.oldPassword);
+        if (error) return res.status(400).json({message: error.details[0].message});
+        const { error1 } = validate(req.body.newPassword);
+        if (error1) return res.status(400).json({message: error1.details[0].message});
         const validPassword = bcrypt.compareSync(req.body.oldPassword, user.password);
         if (!validPassword) return res.status(400).json({ message: 'Invalid old password!'});
         const salt = bcrypt.genSaltSync(10);
