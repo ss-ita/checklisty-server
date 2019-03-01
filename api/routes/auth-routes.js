cd8a690691485dda3c8a781260a311c9fd3ae4f0
Rostyslav Vasyliuk <rostyslaw.wasyliuk@gmail.com>
2019-03-01 12:53:12 +0200

    Minor fix

--- api/routes/auth-routes.js
+++ api/routes/auth-routes.js
@@ -3,6 +3,8 @@ const authController = require('../controllers/auth-controller');
 const router = new express.Router();
 const passport = require('passport');
 
+const url = 'http://localhost:3000';
+
 router.post('/signup', authController.signUp);
 
 router.post('/signin', authController.signIn);
@@ -12,19 +14,19 @@ router.post('/validate', authController.validateUser);
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/google/redirect',
-  passport.authenticate('google', { failureRedirect: 'localhost:3000/auth/signin' }),
+  passport.authenticate('google', { failureRedirect: `${url}/auth/signin` }),
   authController.socialAuth);
 
 router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
 router.get('/facebook/redirect',
-  passport.authenticate('facebook', { failureRedirect: 'localhost:3000/auth/signin' }),
+  passport.authenticate('facebook', { failureRedirect: `${url}/auth/signin` }),
   authController.socialAuth);
 
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 
 router.get('/github/redirect',
-  passport.authenticate('github', { failureRedirect: 'localhost:3000/auth/signin' }),
+  passport.authenticate('github', { failureRedirect: `${url}/auth/signin` }),
   authController.socialAuth);
 
 module.exports = router;
