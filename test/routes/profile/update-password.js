427a593017b9464f8172ea5a4a20f140f5547090
AndriyTykhan <andriytixan@gmail.com>
2019-03-30 15:50:41 +0200

    fix tests

--- test/routes/profile/update-password.js
+++ test/routes/profile/update-password.js
@@ -6,6 +6,7 @@ const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const Joi = require('joi');
 
 const server = require('../../../index');
 
@@ -13,71 +14,81 @@ chai.use(chaiHttp);
 
 const should = chai.should();
 
-describe('Profile', () => {
+describe('Update user password', () => {
   before(() => {
     sinon.stub(mongoose.Model, 'findById')
+      .onFirstCall().throws()
       .returns({ password: 'oldPassword',  select: () => null });
     sinon.stub(mongoose.Model, 'findByIdAndUpdate').returns({})
     sinon.stub(jwt, 'verify').returns({ decoded: { id: '1' } });
     sinon.stub(bcrypt, 'compareSync')
       .onFirstCall().returns(false)
       .onSecondCall().returns(true);
+    sinon.stub(Joi, 'validate')
+      .onFirstCall().returns({ error: { details: [{ message: 'Validation error' }] } })
+      .returns({ error: null });
   });
 
   after(() => {
     mongoose.Model.findById.restore();
     mongoose.Model.findByIdAndUpdate.restore();
     jwt.verify.restore();
+    Joi.validate.restore();
     bcrypt.compareSync.restore();
   });
 
-  describe('Update password', async () => {
-    it('Should reject same old and new password', () => {
-      chai.request(server)
-        .put('/api/profile/updatePassword')
-        .set('access-token', 'token')
-        .send({ oldPassword: 'Password', newPassword: 'Password' })
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Old and new passwords must be different!');
-        });
-    });
-
-    it('Should reject with validation error', () => {
-      chai.request(server)
-        .put('/api/profile/updatePassword')
-        .set('access-token', 'token')
-        .send({ oldPassword: 'OldPassword', newPassword: 'Pas' })
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('"password" length must be at least 6 characters long');
-        });
-    });
-
-    it('Should reject with messageq invalid old password', () => {
-      chai.request(server)
-        .put('/api/profile/updatePassword')
-        .set('access-token', 'token')
-        .send({ oldPassword: 'old', newPassword: 'newPassword' })
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Invalid old password!');
-        });
-    });
-
-    it('Should update password', () => {
-      chai.request(server)
-        .put('/api/profile/updatePassword')
-        .set('access-token', 'token')
-        .send({ newPassword: 'newPassword' })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Password changed!');
-        });
-    });
+  it('Should reject, some error', () => {
+    chai.request(server)
+      .put('/api/profile/updatePassword')
+      .set('access-token', 'token')
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Something go wrong');
+      });
+  });
+  it('Should reject, same old and new password', () => {
+    chai.request(server)
+      .put('/api/profile/updatePassword')
+      .set('access-token', 'token')
+      .send({ oldPassword: 'Password', newPassword: 'Password' })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Old and new passwords must be different!');
+      });
+  });
+  it('Should reject, validation error', () => {
+    chai.request(server)
+      .put('/api/profile/updatePassword')
+      .set('access-token', 'token')
+      .send({ oldPassword: 'OldPassword', newPassword: 'Pas' })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Validation error');
+      });
+  });
+  it('Should reject, invalid old password', () => {
+    chai.request(server)
+      .put('/api/profile/updatePassword')
+      .set('access-token', 'token')
+      .send({ oldPassword: 'old', newPassword: 'newPassword' })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Invalid old password!');
+      });
+  });
+  it('Should update password', () => {
+    chai.request(server)
+      .put('/api/profile/updatePassword')
+      .set('access-token', 'token')
+      .send({ newPassword: 'newPassword' })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Password changed!');
+      });
   });
 });
--- test/routes/profile/update-profile.js
+++ test/routes/profile/update-profile.js
@@ -5,6 +5,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 
 const server = require('../../../index');
 
@@ -14,63 +15,65 @@ const should = chai.should();
 
 describe('Profile', () => {
   before(() => {
-    sinon.stub(mongoose.Model, 'findById').returns({ username: 'JonhDoe', email: 'email@email.com' });
+    sinon.stub(mongoose.Model, 'findById')
+      .onFirstCall().throws()
+      .returns({ username: 'JonhDoe', email: 'email@email.com' });
     sinon.stub(mongoose.Model, 'findByIdAndUpdate')
-      .onFirstCall().returns({ select: () => 'user' })
-      .onSecondCall().throws({ name: 'ValidationError' });
+      .returns({ select: () => 'user' });
     sinon.stub(jwt, 'verify').returns({ decoded: { id: '1' } });
+    sinon.stub(Joi, 'validate')
+      .onFirstCall().returns({ error: { details: [{ message: 'Validation error' }] } })
+      .returns({ error: null });
   });
 
   after(() => {
     mongoose.Model.findById.restore();
     mongoose.Model.findByIdAndUpdate.restore();
     jwt.verify.restore();
+    Joi.validate.restore();
   });
 
-  describe('Profile update', async () => {
-    it('Shoud reject to update with message to fill all fields', () => {
-      chai.request(server)
-        .put('/api/profile/')
-        .set('access-token', 'token')
-        .send({})
-        .end((err, res) => {
-          res.should.have.status(409);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Please fill the form!');
-        });
-    });
-    it('Shoud reject with validate error', () => {
-      chai.request(server)
-        .put('/api/profile/')
-        .set('access-token', 'token')
-        .send({ username: 'JonhDoe', email: 'email' })
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('"email" must be a valid email');
-        });
-    });
-    it('Shoud reject with to change email or username', () => {
-      chai.request(server)
-        .put('/api/profile/')
-        .set('access-token', 'token')
-        .send({ username: 'JonhDoe', email: 'email@email.com' })
-        .end((err, res) => {
-          res.should.have.status(409);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Please change username or email!');
-        });
-    });
-    it('Shoud update profile', () => {
-      chai.request(server)
-        .put('/api/profile/')
-        .set('access-token', 'token')
-        .send({ username: 'NewJonhDoe', email: 'email@email.com' })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Name and email changed!');
-        });
-    });
+  it('Shoud reject, something go wrong', () => {
+    chai.request(server)
+      .put('/api/profile/')
+      .set('access-token', 'token')
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Something go wrong');
+      });
+  });
+  it('Shoud reject, nothing changed', () => {
+    chai.request(server)
+      .put('/api/profile/')
+      .set('access-token', 'token')
+      .end((err, res) => {
+        res.should.have.status(409);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Nothing have changed!');
+      });
+  });
+  it('Shoud reject, validate error', () => {
+    chai.request(server)
+      .put('/api/profile/')
+      .set('access-token', 'token')
+      .send({ username: 'JonhDoe', email: 'email' })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('message');
+        res.body.message.should.eql('Validation error');
+      });
+  });
+  it('Shoud update user profile', () => {
+    chai.request(server)
+      .put('/api/profile/')
+      .set('access-token', 'token')
+      .send({ username: 'JonhDoe', email: 'email' })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message');
+        res.body.should.have.property('updatedUser');
+        res.body.message.should.eql('Name and email changed!');
+      });
   });
 });
